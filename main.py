aef884e13af6393f6bb4ad7027c798b21396fb04
xushnud <xushnudxudoyberdiev25@gmail.com>
2026-06-29 16:21:18 +0500

    birinchi shart

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Git")
+def count_vowels_and_consonants(text: str) -> dict:
+    text = text.lower()
+    unli = 0
+    undosh = 0
+    for word in text:
+        if word == "a" or word == "e" or word == "i" or word == "o" or word == "u":
+         unli += 1
+
+        elif word == word.isalpha():
+           undosh += 0
+    return (f"unli: {unli}, undosh: {undosh}")
+
+
+print(count_vowels_and_consonants("Salom Dunyo!"))
+
+# a, e, i, o, u
